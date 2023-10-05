e0e358130ad5cfcc5243c73e6329a321441f9b6e
alliajagbe <alliajagbe001@gmail.com>
2023-10-05 18:47:11 +0530

    non euclidean distance input reader

--- input_reader.py
+++ input_reader.py
@@ -5,3 +5,11 @@ def read_euclidean_input():
         x, y = map(float, input().split())
         cities.append((x, y))
     return cities
+
+def read_non_euclidean_input():
+    N = int(input())
+    distance_matrix = []
+    for _ in range(N):
+        distances = list(map(float, input().split()))
+        distance_matrix.append(distances)
+    return distance_matrix
