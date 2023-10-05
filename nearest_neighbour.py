bad32caecb103d496361cca820eddbc9ffcf4f97
alliajagbe <alliajagbe001@gmail.com>
2023-10-05 19:03:58 +0530

    nearest neighbour function

---
+++ nearest_neighbour.py
@@ -0,0 +1,26 @@
+from distance import calculate_tour_distance
+
+def nearest_neighbor(matrix):
+    num_cities = len(matrix)
+    tour = [0]  # Start from city 0
+    unvisited_cities = set(range(1, num_cities))
+    
+    current_city = 0
+    best_tour = None
+    best_distance = float('inf')
+
+    while unvisited_cities:
+        nearest_city = min(unvisited_cities, key=lambda city: matrix[current_city][city])
+        tour.append(nearest_city)
+        unvisited_cities.remove(nearest_city)
+        current_city = nearest_city
+        tour_distance = calculate_tour_distance(tour, matrix)
+        
+        if tour_distance < best_distance:
+            best_distance = tour_distance
+            best_tour = tour.copy()
+
+    # Return to the starting city
+    best_tour.append(best_tour[0])
+
+    return best_tour
