332bccde1f53d1eb215c7e2a219b5ddcee9a0b0c
alliajagbe <alliajagbe001@gmail.com>
2023-10-05 20:08:53 +0530

    working nearest neighbour with euclidean

--- nearest_neighbour.py
+++ nearest_neighbour.py
@@ -2,28 +2,27 @@ from distance import calculate_tour_distance
 
 def nearest_neighbor(matrix):
     num_cities = len(matrix)
-    tour = [0]  # Start from city 0
-    unvisited_cities = set(range(1, num_cities))
-    
-    current_city = 0
     best_tour = None
     best_distance = float('inf')
 
-    while unvisited_cities:
-        nearest_city = min(unvisited_cities, key=lambda city: matrix[current_city][city])
-        tour.append(nearest_city)
-        unvisited_cities.remove(nearest_city)
-        current_city = nearest_city
+    for start_city in range(num_cities):
+        tour = [start_city]
+        unvisited_cities = set(range(num_cities))
+        unvisited_cities.remove(start_city)
+        current_city = start_city
 
-        # Calculate the tour distance so far
+        while unvisited_cities:
+            nearest_city = min(unvisited_cities, key=lambda city: matrix[current_city][city])
+            tour.append(nearest_city)
+            unvisited_cities.remove(nearest_city)
+            current_city = nearest_city
+
+        # Return to the starting city
+        tour.append(start_city)
         tour_distance = calculate_tour_distance(tour, matrix)
-        
-        # If this tour is better than the best one so far, update it
+
         if tour_distance < best_distance:
             best_distance = tour_distance
-            best_tour = tour.copy()
+            best_tour = tour
 
-    # Return to the starting city
-    best_tour.append(best_tour[0])
-    
     return best_tour
