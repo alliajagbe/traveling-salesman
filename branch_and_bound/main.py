e60f1bda1fba365d0929f7597a17b4b6b46109a6
alliajagbe <alliajagbe001@gmail.com>
2023-10-14 19:37:41 +0530

    read from input file

--- branch_and_bound/main.py
+++ branch_and_bound/main.py
@@ -1,20 +1,24 @@
+import numpy as np
+
 from distance import calculate_distance_matrix
 from branch_bound import tsp_branch_and_bound 
 
-# Read input
-problem_type = input("EUCLIDEAN or NON-EUCLIDEAN: ").strip()
-n = int(input("Enter Number of Cities: "))
-coordinates = []
 
-# Read coordinates
-for i in range(n):
-    x, y = map(float, input(f"Enter the Coordinates for City {i}:").split())
-    coordinates.append((x, y))
+def file_reader(file):
+    with open(file) as f:
+        problem_type = f.readline().strip()
+        n = int(f.readline().strip())
 
-# Calculate the distance matrix
-distance_matrix = calculate_distance_matrix(coordinates)
-print("Distance Matrix: ")
-print(distance_matrix)
+        if problem_type == 'EUCLIDEAN':
+            coordinates = [tuple(map(float, line.split())) for line in f.readline().strip().split('\n')]
+            distance_matrix = calculate_distance_matrix(coordinates)
+        elif problem_type == 'NON-EUCLIDEAN':
+            distance_matrix = np.array([list(map(float, line.split())) for line in file.readline().strip().split('\n')])
+
+
+        return distance_matrix
+    
+distance_matrix = file_reader('input.txt')
 
 # Example usage:
 tour, tour_length = tsp_branch_and_bound(distance_matrix)
