f03159de04eaafb66009f0e77b5aa2567e9c5950
alliajagbe <alliajagbe001@gmail.com>
2023-10-16 22:50:49 +0530

    updated two opt

--- two-opt/two-opt.py
+++ two-opt/two-opt.py
@@ -15,9 +15,9 @@ def read_input(file_path):
 
 def calculate_tour_length(tour, distances):
     total_length = 0.0
-    n = len(tour)
     for i in range(len(tour) - 1):
-        total_length += distances[tour[i]][tour[(i+1) % n]]
+        total_length += distances[tour[i]][tour[i+1]]
+    total_length += distances[tour[-1]][tour[0]]  # Return to the starting city
     return total_length
 
 def two_opt(tour, distances):
