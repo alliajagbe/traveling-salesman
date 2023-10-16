88b42fab543f541ea1425f83b1d38ba48a240268
alliajagbe <alliajagbe001@gmail.com>
2023-10-16 23:49:20 +0530

    tour length calculation

--- genetic_algorithm/genetic.py
+++ genetic_algorithm/genetic.py
@@ -18,8 +18,9 @@ def read_input(file_path):
 def calculate_tour_length(tour, distances):
     total_length = 0.0
     n = len(tour)
-    for i in range(n):
-        total_length += distances[tour[i]][tour[(i + 1) % n]]
+    for i in range(n - 1):
+        total_length += distances[tour[i]][tour[i + 1]]
+    total_length += distances[tour[-1]][tour[0]]  # Return to the starting city
     return total_length
 
 def generate_initial_population(N, population_size):
