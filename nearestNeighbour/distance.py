e5cd3e715ddfaf34b0257ee73869e517621d21fa
alliajagbe <alliajagbe001@gmail.com>
2023-10-18 09:42:44 +0530

    removed unused function

--- nearestNeighbour/distance.py
+++ nearestNeighbour/distance.py
@@ -1,9 +1,3 @@
-import math 
-def calculate_euclidean(city1, city2):
-    x1, y1 = city1
-    x2, y2 = city2
-    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
-
 def calculate_tour_distance(tour, distance_matrix):
     total_distance = 0
     for i in range(len(tour) - 1):
