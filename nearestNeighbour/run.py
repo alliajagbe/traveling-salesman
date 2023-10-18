8ab7d5e438c917a4da7c63022126678693fcf0bc
alliajagbe <alliajagbe001@gmail.com>
2023-10-18 09:41:21 +0530

    updated file

--- nearestNeighbour/run.py
+++ nearestNeighbour/run.py
@@ -1,39 +1,13 @@
-from input_reader import read_euclidean_input, read_non_euclidean_input
+from input_reader import read_input
 from distance import calculate_euclidean, calculate_tour_distance
 from nearest_neighbour import nearest_neighbor
 import numpy as np
 
 def main():
-    input_type = input("Enter input type (EUCLIDEAN or NON-EUCLIDEAN):").strip()
-    
-    if input_type == "EUCLIDEAN":
-        cities = read_euclidean_input()
-        print("CITIES:")
-        print(cities)
-        num_cities = len(cities)
-        print(f"Number of cities: {num_cities}")
-        distance_matrix = np.zeros((num_cities, num_cities))
-        
-        # Calculate distances for the Euclidean case
-        for i in range(num_cities):
-            for j in range(i+1, num_cities):
-                distance_matrix[i][j] = calculate_euclidean(cities[i], cities[j])
-                distance_matrix[j][i] = distance_matrix[i][j]
+    input_file = "200.txt"
+    tsp_type, N, cities, distances = read_input(input_file)
 
-        print("DISTANCE MATRIX:")
-        print(distance_matrix)
-        
-        # Display coordinates
-        print("Coordinates of Cities:")
-        for i, city in enumerate(cities):
-            print(f"City {i}: {city}")
-    elif input_type == "NON-EUCLIDEAN":
-        distance_matrix = read_non_euclidean_input()
-    else:
-        print("Invalid input type. Please use EUCLIDEAN or NON-EUCLIDEAN.")
-        return
-
-    best_tour = nearest_neighbor(distance_matrix)
+    best_tour = nearest_neighbor(distances)
     print("PATH REPRESENTATION OF TOUR:")
     print(" ".join(map(str, best_tour)))
 
